72987eaf91e8c683f969d7e8fce84c117dc6b2c7
Victor Ribero <victor.ribero3@gmail.com>
2018-04-16 17:19:54 +0200

    Created styles for the phone layout

--- src/Components/Phone/index.tsx
+++ src/Components/Phone/index.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react';
 import Keyboard from "../Keyboard";
+import glamorous from "glamorous";
+
+
+const PhoneStyles = glamorous.main({
+  'border': '2px solid #e0e1e2',
+  'border-radius': '6px',
+  'background': 'white',
+  'padding-top': '20px',
+  'padding-bottom': '30px',
+  'box-shadow': '0 0 1em 0.25em rgba(0,0,0,0.2)',
+});
 
 const Phone = (props:any) => {
   return (
-    <div className={'phone'}>
+    <PhoneStyles>
+
       {props.children}
       <Keyboard />
-    </div>
+
+    </PhoneStyles>
   );
 };
 export default Phone;
