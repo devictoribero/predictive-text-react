ee3a7d306af61ea7493ebb1fe8290d5a5dc4409f
Victor Ribero <victor.ribero3@gmail.com>
2018-04-18 23:44:54 +0200

    changed message

--- src/ConversationExample.ts
+++ src/ConversationExample.ts
@@ -6,8 +6,8 @@ const conversationExample = {
         avatar: 'https://avatars0.githubusercontent.com/u/16169890?s=400&v=4',
       },
       data: [
-        'To start, start typing on the chatbox',
-        'Don\'t forget to check my website www.victorribero.com',
+        'Press some numbers to start',
+        'Rembember to check my website/portfolio www.victorribero.com',
       ],
     }
   ],
